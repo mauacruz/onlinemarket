71f42534766c41e4187ec70c2feb9b809e2dd984
Mauricio Arruda Cruz <mauacruz@gmail.com>
2016-02-16 00:03:13 -0200

    Commit Resolucao M6Ex1 parcial - 16.02.2016

--- module/Market/autoload_classmap.php
+++ module/Market/autoload_classmap.php
@@ -3,6 +3,9 @@
 return array(
     'Market\Module'                             => __DIR__ . '/Module.php',
     'Market\Controller\IndexController'         => __DIR__ . '/src/Market/Controller/IndexController.php',
+    'Market\Controller\PostController'          => __DIR__ . '/src/Market/Controller/PostController.php',
+    'Market\Controller\ViewController'          => __DIR__ . '/src/Market/Controller/ViewController.php',
+    'Market\Factory\PostControllerFactory'      => __DIR__ . '/src/Market/Factory/PostControllerFactory.php',
     'ZendSkeletonModuleTest\Framework\TestCase' => __DIR__ . '/tests/Market/Framework/TestCase.php',
     'ZendSkeletonModuleTest\SampleTest'         => __DIR__ . '/tests/Market/SampleTest.php',
 );
