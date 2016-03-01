e36a7663c566a2a4c0f029631f98d68b2bfc0994
Mauricio <mauacruz@gmail.com>
2016-03-01 12:00:28 -0300

    Resolucao M7Ex1 - 01.03.2016

--- module/Market/config/module.config.php
+++ module/Market/config/module.config.php
@@ -33,9 +33,9 @@ return array(
                         'action' => 'index'
                     ),
                 ),
-                'may-terminate' => true,
+                'may_terminate' => true,
 	   				
-			    'child-routes' => array(
+			    'child_routes' => array(
 			    		
 			    	'index' => array(	
 						'type' => 'Segment',
@@ -72,51 +72,15 @@ return array(
                 )
             ),
             'market-post' => array(
-	        'type' => 'Literal',
+	        	'type' => 'Literal',
                 'options' => array(
-                   'route' => '/market/post',
-		    'defaults' => array(
-    			'controller' => 'market-post-controller',
-        		'action' => 'index'
-                   )
+                	'route' => '/market/post',
+		    		'defaults' => array(
+    					'controller' => 'market-post-controller',
+        				'action' => 'index'
+                   	)
                 )
             ),
-
-          /*  'market' => array(
-                'type'    => 'Literal',
-                'options' => array(
-                    // Change this to something specific to your module
-                    'route'    => '/market',
-                    'defaults' => array(
-                        // Change this value to reflect the namespace in which
-                        // the controllers for your module are found
-                        //'__NAMESPACE__' => 'Market\Controller',
-                        'controller'    => 'market-index-controller',
-                        'action'        => 'index',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    // This route is a sane default when developing a module;
-                    // as you solidify the routes for your module, however,
-                    // you may want to remove it and replace it with more
-                    // specific routes.
-                    'default' => array(
-                        'type'    => 'Segment',
-                        'options' => array(
-                            'route'    => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            'defaults' => array(
-                            ),
-                        ),
-                    ),
-                ),
-            
-            ),
-        */  
         ),
     ),
     'view_manager' => array(
