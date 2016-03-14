7fd68f2fd291dc5c9e9a7bf083c4abab9c6db153
Mauricio Arruda Cruz <mauacruz@gmail.com>
2016-03-13 21:31:02 -0300

    Resolução M9Ex1 - 13.03.2016 (menos ex opcional)

---
+++ module/Market/src/Market/Factory/PostFilterFactory.php
@@ -0,0 +1,16 @@
+<?php
+namespace Market\Factory;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Market\Form\PostFilter;
+
+class PostFilterFactory implements FactoryInterface {
+	
+	public function createService(ServiceLocatorInterface $sm){
+		$filter = new PostFilter();
+		$filter->setCategories($sm->get('categories'));
+		$filter->buildFilter();
+		return $filter;
+	}
+}
